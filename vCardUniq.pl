e4d9d2ba20e5e704c1c45fd489d371f0cc7b4691
rozie <rozie@poczta.onet.pl>
2016-09-27 19:10:45 +0200

    perltidy applied

--- vCardUniq.pl
+++ vCardUniq.pl
@@ -3,26 +3,26 @@
 use strict;
 use warnings;
 
-my%vcards=();
-my $vcard="";
+my %vcards = ();
+my $vcard  = "";
 
-while (<>){
-	if (/^BEGIN\:VCARD/){
-		$vcard="";
-		print "Begin\n";
-	}
-	elsif (/^END\:VCARD/){
-		print "End\n";
-		$vcards{$vcard}=1;
-	}
-	else {
-		print "Body\n";
-		$vcard.=$_;
-	}
+while (<>) {
+    if (/^BEGIN\:VCARD/) {
+        $vcard = "";
+        print "Begin\n";
+    }
+    elsif (/^END\:VCARD/) {
+        print "End\n";
+        $vcards{$vcard} = 1;
+    }
+    else {
+        print "Body\n";
+        $vcard .= $_;
+    }
 }
 
-foreach $vcard (keys %vcards){
-	print "BEGIN:VCARD\n";
-	print "$vcard";
-	print "END:VCARD\n";
+foreach $vcard ( keys %vcards ) {
+    print "BEGIN:VCARD\n";
+    print "$vcard";
+    print "END:VCARD\n";
 }
